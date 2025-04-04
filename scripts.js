0b79ab868563414e4961a42daa08990786c95050
maggiehuang19 <mh5919@nyu.edu>
2025-04-03 20:05:40 -0400

    moving my code

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById("btn1").addEventListener("click", function() {
+    window.open("https://www.amazon.com/hz/wishlist/ls/3DC6PINX6KEI5?ref_=wl_share", "_blank");
+  });
+  
+  document.getElementById("btn2").addEventListener("click", function() {
+    window.open("https://apresnail.com", "_blank");
+  });
+  
+  document.getElementById("btn3").addEventListener("click", function() {
+    window.open("https://dndgel.com", "_blank");
+  });
